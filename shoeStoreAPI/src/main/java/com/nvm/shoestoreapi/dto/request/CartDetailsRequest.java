554d336548521d22e074f1b68515e7e6d76c143c
manhnv01 <manhnv291201@gmail.com>
2024-04-07 17:56:23 +0700

    fix

--- shoeStoreAPI/src/main/java/com/nvm/shoestoreapi/dto/request/CartDetailsRequest.java
+++ shoeStoreAPI/src/main/java/com/nvm/shoestoreapi/dto/request/CartDetailsRequest.java
@@ -5,12 +5,17 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.Min;
+
+import static com.nvm.shoestoreapi.util.Constant.QUANTITY_MUST_BE_GREATER_THAN_0;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class CartDetailsRequest {
     private Long id;
+    @Min(value = 1, message = QUANTITY_MUST_BE_GREATER_THAN_0)
     private Integer quantity;
     private Long productDetailsId;
 }
